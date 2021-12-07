1e92b863123866ead4974774150cfba07d58b4ca
Руслан Зиянгиров <ziyangirovr@mail.ru>
2021-12-07 22:11:20 +0300

    Fixed problems

--- src/main/java/org/openjfx/EndController.java
+++ src/main/java/org/openjfx/EndController.java
@@ -5,4 +5,8 @@ import javafx.scene.input.MouseEvent;
 import java.io.IOException;
 
 public class EndController {
+
+    public void goMainScene(MouseEvent mouseEvent) throws IOException {
+        App.setRoot("primary");
+    }
 }
