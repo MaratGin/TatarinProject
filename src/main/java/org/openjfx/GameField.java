53a5383fd6911446f449a810c21a81a1eeb6345e
Руслан Зиянгиров <ziyangirovr@mail.ru>
2021-12-13 22:27:18 +0300

    Add timer

--- src/main/java/org/openjfx/GameField.java
+++ src/main/java/org/openjfx/GameField.java
@@ -1,4 +1,42 @@
 package org.openjfx;
 
-public class GameField {
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class GameField implements Initializable {
+
+    @FXML
+    public Label timer_str;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        setTimer();
+    }
+
+    public void setTimer() {
+        AtomicInteger time = new AtomicInteger(60);
+        Timeline timeline = new Timeline (
+                new KeyFrame (
+                        Duration.millis(1000), //1000 мс * 60 сек = 1 мин
+                        ae -> {
+                            timer_str.setText("Осталось времени: "+ time);
+                            time.getAndDecrement();
+                            System.out.println("Жопа");
+                        }
+                )
+        );
+
+        timeline.setCycleCount(60); //Ограничим число повторений
+        timeline.play();
+    }
 }
+
+
+
--- src/main/java/org/openjfx/ThirdController.java
+++ src/main/java/org/openjfx/ThirdController.java
@@ -1,11 +1,17 @@
 package org.openjfx;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ThirdController {
+
+
 
     public void goBack(MouseEvent mouseEvent) throws IOException {
         App.setRoot("primary");
@@ -14,4 +20,6 @@ public class ThirdController {
     public void goPlay(ActionEvent actionEvent) throws IOException {
         App.setRoot("gameField");
     }
+
 }
+
